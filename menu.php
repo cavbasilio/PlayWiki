a619ef7dcb6b9dca3db35f4e6fa7edd291475974
Jordan <jordan.basilio1@hotmail.com>
2024-01-29 10:50:32 -0300

    menu.php

---
+++ menu.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PLAY WIKI</title>
+
+        <!-- Inclua os links para os estilos do FontAwesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+            integrity="sha384-H4MD6mqIauG+LkQ3I5aFuYPQHY7IxfQ0JMKuAP46PI4lnm+5RF4f0R95uXzWv2v1" crossorigin="anonymous">
+        <!-- Adicione o link para os estilos do Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Link para fontes da GOOGLE--> 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik+Maps&display=swap" rel="stylesheet">
+                            <!-- AQUI ENTRA O CSS-->
+        <link rel="stylesheet" href="css/style.css">
+       
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ffa500;">
+        <a class="navbar-brand" href="index.php">
+            <img src="img/logoPlay.png" id="imgplayfibraNav"
+                class="d-inline-block align-top">
+        </a>
+
+        <!-- Botão do hamburguer para telas menores -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Div para agrupar itens do menu -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                    <!-- Links para as páginas com ícones -->
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="atendimento.php"><i class="fas fa-headset"></i>Atendimento |</a>
+                    </li>  
+                    <li class="nav-item">
+                        <a class="nav-link" href="tecnico.php"><i class="fas fa-wrench">Técnico  |</i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="fusao.php"><i class="fas fa-code-branch"></i>Fusão  |</a>
+                    </li>    
+                    <li class="nav-item">
+                        <a class="nav-link" href="financeiro.php"><i class="fas fa-dollar-sign">Financeiro  |</i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="noc.php"><i class="fas fa-descktop">NOC </i></a>
+                    </li>
+                            <!-- BOTÃO DE SAIR -->
+                    <button type="button" class="btn btn-info" style="width: 50px; height: 40px; background-color: #a46b14 ; font-size: 6pt;" data-toggle="modal" data-target="#exampleModal">
+                    Sair
+                    </button>
+            </ul>
+        </div>
+    </nav>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>

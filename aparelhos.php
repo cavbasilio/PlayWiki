150aec99eac7b1668880d61c9247f024bdcce9b5
Jordan Basilio <jordan.basilio@gmail.com>
2024-05-13 10:15:38 -0300

    adicionando a tela onde terá os botões do aparelho

---
+++ aparelhos.php
@@ -0,0 +1,28 @@
+<?php 
+    include 'menu.php';
+?>
+
+<body>
+
+<div class="container d-flex flex-column"style="background-color:transparent;" >
+    <ul class="list-group">
+        <li class="list-group-item d-flex flex-column align-items-center text-center" style="background-color:transparent;">
+            <div >
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Maxprint-Branca(MAXFIBER 1200G2)</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Maxprint Preta(MAXFIBER 1200GM FXS)</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Raisecom DualBand</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Nokia G-140W(AZUL)</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Nokia G-240w-c (VERDE)</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Shoreline</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Intelbras</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Huawei PRETA(HG8145)</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">AC Unee by Stavix MP-G421R</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">D-Link</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Tp-link</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">ZTE 670L</button>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">ZTE AX3000 F6600p</button>
+            </div>
+        </li>
+    </ul>
+</div>
+</body>

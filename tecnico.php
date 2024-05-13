f4ab5994b60d4a30337f23726b7d3fb3f03e34f2
Jordan Basilio <jordan.basilio@gmail.com>
2024-05-13 10:31:10 -0300

    Adicionando alterações para a tela de aparelhos

--- tecnico.php
+++ tecnico.php
@@ -7,21 +7,22 @@
 <div class="container d-flex flex-column"style="background-color:transparent;" >
     <ul class="list-group">
         <li class="list-group-item d-flex flex-column align-items-center text-center" style="background-color:transparent;">
-            <div >
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Maxprint-Branca(MAXFIBER 1200G2)</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Maxprint Preta(MAXFIBER 1200GM FXS)</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Raisecom DualBand</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Nokia G-140W(AZUL)</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Nokia G-240w-c (VERDE)</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Shoreline</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Intelbras</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Huawei PRETA(HG8145)</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">AC Unee by Stavix MP-G421R</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">D-Link</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Tp-link</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">ZTE 670L</button>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">ZTE AX3000 F6600p</button>
+            <div id="caixa-supervisor">
+                <!--FOTO + APRESENTAÇÃO DA EQUIPE DO SETOR TÉCNICO -->
+
             </div>
+            <div id="caixa-tecnico1">
+                <!--FOTO + APRESENTAÇÃO DA EQUIPE DO SETOR TÉCNICO -->
+                
+            </div>
+            <div id="caixa-tecnico2">
+                <!--FOTO + APRESENTAÇÃO DA EQUIPE DO SETOR TÉCNICO -->
+                
+            </div>
+
+            <a href="aparelhos.php">
+                <button id="btn-aparelho">APARELHOS (ONU)</button>
+            </a>
         </li>
     </ul>
 </div>

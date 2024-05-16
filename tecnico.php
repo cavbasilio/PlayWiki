bb63c63840f62baa2f07b61039041b7aa631a5df
Jordan Basilio <jordan.basilio@gmail.com>
2024-05-16 12:17:52 -0300

    alterando o css  e melhorando a tela dos tecnicos

--- tecnico.php
+++ tecnico.php
@@ -3,140 +3,80 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
 
-<title>Ambiente Técnico</title>
-</head>
-<body>
-    <div class="container" style="justify-content: center; margin-top:50px; font-size: 40px; font-weight: bold; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
-        EQUIPE TÉCNICA
+  
+    <div class="tela-tecnico">
+      <div class="div">
+        <div class="headline">
+          <div class="text-wrapper">Renan</div>
+      </div>
+
+      <div class="overlap-group">
+        <div class="div-wrapper">
+          <div class="text-wrapper">Victor</div>
+        </div>
+          <img class="hero-image" src="img/Victor.jpeg"/>
+      </div>
+
+        <div class="overlap">
+          <div class="div-wrapper">
+            <div class="text-wrapper">Emanuel</div>
+        </div>
+
+          <img class="img" src="img/emanuel.jpg"/>
+
+        </div>
+
+        <div class="overlap-2">
+          <div class="headline-2">
+            <div class="text-wrapper">Josiel</div>
+          </div>
+          <img class="hero-image-2" src="img/Josiel.jpeg"/>
+        </div>
+      
+        <div class="overlap-3">
+          <div class="headline-3">
+            <div class="text-wrapper">nome_tecnico2</div>
+          </div>
+            <img class="hero-image-3" src="img/hero-image.png" />
+        </div>
+
+
+        <div class="headline-4">
+          <div class="text-wrapper">Rodrigo</div>
+        </div>
+
+
+          <img class="hero-image-4" src="img/guida.jpeg" />
+
+          <div> <img class="hero-image-5" src="img/renan.jpeg" /></div>
+          <img class="hero-image-6" src="img/Rodrigo.jpeg" />
+
+        <div class="headline-5">
+          <div class="text-wrapper">Leonardo</div>
+        </div>
+        <img class="hero-image-7" src="img/leonardoTec.jpg"/>
+        <div class="overlap-4">
+          <div class="headline-3">
+            <div class="text-wrapper">Luciel</div>
+          </div>
+            <img class="hero-image-3" src="img/luciel.jpg"/>
+        </div>
+
+        <div class="headline-6">
+          <div class="text-wrapper-2">Thiago Guida</div>
+        </div>
+        <img class="vector" src="img/vector-1.svg"/>
+        <div class="text-wrapper-3">COMPOSIÇÃO DA EQUIPE TÉCNICA</div>
+      </div>
     </div>
-   
-    <div class="container">
-    <div class=""> Atua na supervisão dos técnicos.</div><div class="cracha">
-            <div class="avatar">
-                <img src="./img/guida.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">THIAGO GUIDA</div>
-            <div class="cargo">Supervisor</div>
-        </div>
-
-
-        <div class="barra-vertical"></div>
-        <div class=""> Especialista em visitas técnicas voltadas para lentidão e orintação ao cliente sobre as funcionalidades do sinal wi-fi e outros tópicos referentes a conectividade. </div>
-        <div class="cracha">
-            <div class="avatar">
-                <img src="./img/renan.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">RENAN</div>
-            <div class="cargo">Técnico</div>
-        </div>
-    </div>
-
-    <div class="container">
-    <div class=""> Instalador/reparador de telecom. Profisional da área com muita bagagem voltada para a área de telecom.</div>
-    <div class="cracha">
-            <div class="avatar">
-                <img src="./img/Josiel.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">JOSIEL</div>
-            <div class="cargo">Técnico</div>
-        </div>
-        
-        <div class="barra-vertical"></div>
-        <div class=""> Instalador/reparador de telecom. Profissional da área de telecom, altamente eficiente em instalações.</div>
-         <div class="cracha">
-            <div class="avatar">
-                <img src="./img/Victor.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">Victor</div>
-            <div class="cargo">Técnico</div>
-        </div>
-    </div>
-
-    <div class="container">
-    <div class=""> Instalador/reparador de telecom. Profissional da área de telecom, altamente eficiente em instalações.</div>
-     <div class="cracha">
-            <div class="avatar">
-                <img src="./img/Victor.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">Victor</div>
-            <div class="cargo">Técnico</div>
-        </div>
-
-        <div class="barra-vertical"></div>
-         <div class=""> Instalador/reparador de telecom. Profissional da área de telecom, altamente eficiente em instalações.</div>    
-            <div class="cracha">
-                <div class="avatar">
-                    <img src="./img/Victor.jpeg" alt="Avatar">
-                </div>
-                <div class="nome">Victor</div>
-                <div class="cargo">Técnico</div>
-            </div>
-
-
-    </div>
-
-    <div class="container">
-            <div class="cracha">
-                <div class="avatar">
-                    <img src="./img/Victor.jpeg" alt="Avatar">
-                </div>
-                <div class="nome">Victor</div>
-                <div class="cargo">Técnico</div>
-            </div>
-
-        <div class="barra-vertical"></div>
-            <div class="cracha">
-                <div class="avatar">
-                    <img src="./img/Victor.jpeg" alt="Avatar">
-                </div>
-                <div class="nome">Victor</div>
-                <div class="cargo">Técnico</div>
-            </div>
-    
-    
-    </div>
-
-    <div class="container">
-        <div class="cracha">
-            <div class="avatar">
-                <img src="./img/Victor.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">Victor</div>
-            <div class="cargo">Técnico</div>
-        </div>
-        <div class="barra-vertical"></div>
-        <div class="cracha">
-            <div class="avatar">
-                <img src="./img/Victor.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">Victor</div>
-            <div class="cargo">Técnico</div>
-        </div>
-
-    </div>
-
-    <div class="container">
-        <div class="cracha">
-            <div class="avatar">
-                <img src="./img/Victor.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">Victor</div>
-            <div class="cargo">Técnico</div>
-        </div>
-        <div class="barra-vertical"></div>
-        <div class="cracha">
-            <div class="avatar">
-                <img src="./img/Victor.jpeg" alt="Avatar">
-            </div>
-            <div class="nome">RENAN</div>
-            <div class="cargo">Técnico</div>
-        </div>
-
-    </div>
-</body>
+  </body>
 </html>
